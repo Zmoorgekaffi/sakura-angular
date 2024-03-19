8826ea5912be2d762e3d82d55070597cc32bf4b5
andre <andre.karyoti.schuro@gmail.com>
2024-03-19 22:55:39 +0100

    added atfsection

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { LandingPageComponent } from './ladingPage/landingPage.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
---
+++ sakura/src/app/landing-page/atfsection/atfsection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-atfsection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './atfsection.component.html',
+  styleUrl: './atfsection.component.scss'
+})
+export class AtfsectionComponent {
+
+}
